dfbe5a81dd80908d3e144490f5a4067a988ea8ef
Náthally <gaviaonathally2006@gmail.com>
2026-09-15 08:52:15 -0300

    Adiciona aula 06 e exercício 2 da aula 05

---
+++ Aula05/exercicio2.py
@@ -0,0 +1,20 @@
+from collections import Counter
+x = int(input())
+
+shoes_array = input().split()
+shoes_count = Counter(shoes_array) 
+n = int(input())
+
+payment = 0
+
+
+for i in range(n):
+    shoes_value = input().split()
+    shoes = (shoes_value[0])
+    value = int (shoes_value[1])
+    if shoes_count[shoes] > 0:
+        shoes_count[shoes] = shoes_count[shoes] - 1
+        payment = payment + value
+        print(f"O sapato {shoes} tem no estoque! Estoque atual{shoes_count[shoes]}")
+
+print(payment)        
