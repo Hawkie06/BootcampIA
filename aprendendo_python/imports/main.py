a80dc2d2a0fe1091cdd1bf6a552b547520af033f
Náthally <gaviaonathally2006@gmail.com>
2026-09-17 10:58:05 -0300

    Adiciona pasta para o conteúdo de lógica de programação em Python

--- aprendendo_python/imports/main.py
+++ aprendendo_python/imports/main.py
@@ -1,5 +1,5 @@
 #import top_classe
-from classes.top_classe import Aluno #Se tivesse outra classe, seria algo como Aluno, OutraClasse
+from aprendendo_python.imports.classes.top_classe import Aluno #Se tivesse outra classe, seria algo como Aluno, OutraClasse
 #Tudo que está definido dentro de outro arquivo é possível importar
 
 # Turma = { "alunos": [Aluno("Náthally", [10,10,10]),  Aluno("João", [20,10,50])]}
